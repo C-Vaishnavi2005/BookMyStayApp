f47062d17b87bf07d9c4303b790cea62e7f3ce82
C-Vaishnavi2005 <vaishnavi.csekar@gmail.com>
2026-03-12 09:02:47 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,8 @@
+public class BookMyStayApp {
+    public static void main(String[] args) {
+        String appName = "Book My Stay";
+        String version = "Version 1.0";
+        System.out.println("Welcome to " + appName);
+        System.out.println(version);
+    }
+}
